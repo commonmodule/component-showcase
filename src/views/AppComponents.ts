30e9018c7f3404cfc5aa63f2b24619067795634e
yjgaia <yjdev88@gmail.com>
2025-05-19 16:48:44 +0900

    Enhance AppComponents view with button components and structured layout

--- src/views/AppComponents.ts
+++ src/views/AppComponents.ts
@@ -1,9 +1,35 @@
 import { AppRoot, el, View } from "@commonmodule/app";
+import { Button, ButtonType } from "@commonmodule/app-components";
+import { AddIcon } from "@commonmodule/svg-icons";
 
 export default class AppComponents extends View {
   constructor() {
     super();
-    this.container = el(".app-components-view", "App Components Showcase")
-      .appendTo(AppRoot);
+    this.container = el(
+      ".showcase-view.app-components-view",
+      el("h1", "App Components Showcase"),
+    ).appendTo(AppRoot);
+
+    this.container.append(
+      el(
+        "section.form-components",
+        el("h2", "Form Components"),
+        el(
+          "section.buttons",
+          el("h3", "Buttons"),
+          el(
+            "section",
+            el("h4", "Basic Button"),
+            el(
+              ".button-container",
+              new Button({ type: ButtonType.Text, title: "Text" }),
+              new Button({ type: ButtonType.Contained, title: "Contained" }),
+              new Button({ type: ButtonType.Outlined, title: "Outlined" }),
+              new Button({ type: ButtonType.Icon, icon: new AddIcon() }),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
