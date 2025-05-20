9a445cd157506cb99bb57ca948501d4b96540523
yjgaia <yjdev88@gmail.com>
2025-05-20 21:11:35 +0900

    feat: Enhance RichTextEditorView with Markdown and Rich Text editors; add SVGIconsView for dynamic icon rendering

--- src/views/RichTextEditorView.ts
+++ src/views/RichTextEditorView.ts
@@ -1,4 +1,6 @@
-import { AppRoot, el, View } from "@commonmodule/app";
+import { AppRoot, Dom, el, View } from "@commonmodule/app";
+import { MarkdownConverter } from "@commonmodule/markdown";
+import { MarkdownEditor, RichTextEditor } from "@commonmodule/markdown-editor";
 
 export default class RichTextEditorView extends View {
   constructor() {
@@ -7,5 +9,32 @@ export default class RichTextEditorView extends View {
       ".showcase-view.rich-text-editor-view",
       el("h1", "Rich Text Editor"),
     ).appendTo(AppRoot);
+
+    let markdownEditor: MarkdownEditor;
+    let markdownDocument: Dom;
+    let richTextEditor: RichTextEditor;
+    let richTextDocument: Dom;
+
+    this.container.append(
+      el(
+        "section.markdown-editor-section",
+        el("h2", "Markdown Editor"),
+        markdownEditor = new MarkdownEditor(),
+        el("h3", "Preview"),
+        markdownDocument = el(".markdown-document"),
+      ),
+      el(
+        "section.rich-text-editor-section",
+        el("h2", "Rich Text Editor"),
+        richTextEditor = new RichTextEditor(),
+        el("h3", "Preview"),
+        richTextDocument = el(".markdown-document"),
+      ),
+    );
+
+    markdownEditor.on("contentChanged", async (newContent: string) => {
+      markdownDocument.htmlElement.innerHTML = await MarkdownConverter
+        .convertMarkdownToHtml(newContent);
+    });
   }
 }
